6545f1e5caba6a8ea5ecae72fdc6b5ee41ac49d2
jb881122 <joeybeattie5@gmail.com>
2024-04-22 22:08:08 +0000

    cmd_list: "Use" the cmd_list_main() arguments to make compilers happy
    Change-Id: I32571f7ed2888d1ce51034d4492ac943c5c7ef78

--- src/cmd_list.c
+++ src/cmd_list.c
@@ -5,6 +5,10 @@
 #include <stddef.h>
 
 int cmd_list_main(int argc, char *argv[]) {
+    /* Unused arguments */
+    (void)argc;
+    (void)argv;
+
     printf("Supported bootloaders: ");
     for(size_t i = 0; i < num_configs; i++) {
         printf("%s", configs[i].name);
