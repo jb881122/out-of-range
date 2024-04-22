985d52555a9ded75c9ffd7583bef0604b69ed5ab
jb881122 <joeybeattie5@gmail.com>
2024-04-22 22:07:19 +0000

    asm: Comply with C99, don't redeclare typedef
    Change-Id: Ia935419f7807ff2a58354fa7b59f554d9e5b1ee4

--- src/asm.h
+++ src/asm.h
@@ -1,9 +1,9 @@
 #ifndef _ASM_H
 #define _ASM_H
 
-#include <stddef.h>
+#include "config.h"
 
-typedef struct bootloader_config bootloader_config;
+#include <stddef.h>
 
 char *make_exploit_ramdisk(char *aboot_code, size_t aboot_code_len,
         bootloader_config *config, size_t input_len, size_t *output_len);
